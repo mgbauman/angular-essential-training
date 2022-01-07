f2c40dddb301bbd14dc41e2ab80fe1fe2a86c3dc
Matt Bauman <mattgbauman@gmail.com>
2022-01-07 07:16:00 -0700

    Created custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './category-list.pipe';
 
 @NgModule({
   imports: [
@@ -11,6 +12,7 @@ import { FavoriteDirective } from './favorite.directive';
   ],
   declarations: [
     AppComponent,
+    CategoryListPipe,
     MediaItemComponent,
     MediaItemListComponent,
     FavoriteDirective
